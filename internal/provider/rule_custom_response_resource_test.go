43edf4bc905d8a0def851f942bbd9732818f46a8
Steve Worley <sj.worley88@gmail.com>
2025-06-04 16:42:48 +1000

    Fix: test cases.

--- internal/provider/rule_custom_response_resource_test.go
+++ internal/provider/rule_custom_response_resource_test.go
@@ -48,23 +48,24 @@ var customResponseResponse = map[string]interface{}{
 func mockCustomResponseServer(t *testing.T, organizationID string, ruleID string) {
 	httpmock.Activate()
 	baseUrl := "https://dashboard.quantcdn.io/api/v2"
+	uuid := "96e4f4f6-211a-4f4b-b7fb-03985df56dad"
 
 	httpmock.RegisterNoResponder(func(req *http.Request) (*http.Response, error) {
-		t.Logf("Request: %s", req.URL)
+		t.Logf("No responder found for request: %s", req.URL)
 		return httpmock.NewStringResponse(404, "Not Found"), nil
 	})
 
-	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/organizations/%s/custom-responses", baseUrl, organizationID),
+	httpmock.RegisterResponder("POST", fmt.Sprintf("%s/organizations/%s/projects/%s/rules/custom-response", baseUrl, organizationID, organizationID),
 		func(req *http.Request) (*http.Response, error) {
 			return httpmock.NewJsonResponse(200, customResponseResponse)
 		})
 
-	httpmock.RegisterResponder("GET", fmt.Sprintf("%s/organizations/%s/custom-responses/%s", baseUrl, organizationID, ruleID),
+	httpmock.RegisterResponder("GET", fmt.Sprintf("%s/organizations/%s/projects/%s/rules/custom-response/%s", baseUrl, organizationID, organizationID, uuid),
 		func(req *http.Request) (*http.Response, error) {
 			return httpmock.NewJsonResponse(200, customResponseResponse)
 		})
 
-	httpmock.RegisterResponder("PUT", fmt.Sprintf("%s/organizations/%s/custom-responses/%s", baseUrl, organizationID, ruleID),
+	httpmock.RegisterResponder("PATCH", fmt.Sprintf("%s/organizations/%s/projects/%s/rules/custom-response/%s", baseUrl, organizationID, organizationID, uuid),
 		func(req *http.Request) (*http.Response, error) {
 			body, err := io.ReadAll(req.Body)
 			if err != nil {
@@ -72,28 +73,62 @@ func mockCustomResponseServer(t *testing.T, organizationID string, ruleID string
 			}
 
 			var requestBody struct {
-				RuleId string `json:"rule_id"`
-				ActionConfig struct {
-					CustomResponseStatusCode int32 `json:"custom_response_status_code"`
-					CustomResponseBody string `json:"custom_response_body"`
-				} `json:"action_config"`
+				CustomResponseStatusCode int32  `json:"custom_response_status_code"`
+				CustomResponseBody      string `json:"custom_response_body"`
 			}
 
 			if err := json.Unmarshal(body, &requestBody); err != nil {
 				return httpmock.NewStringResponse(400, "Invalid JSON"), nil
 			}
 
-			customResponseResponse["action_config"] = map[string]interface{}{
-				"custom_response_status_code": requestBody.ActionConfig.CustomResponseStatusCode,
-				"custom_response_body": requestBody.ActionConfig.CustomResponseBody,
+			response := customResponseResponse
+			response["action_config"] = map[string]interface{}{
+				"custom_response_status_code": requestBody.CustomResponseStatusCode,
+				"custom_response_body":        requestBody.CustomResponseBody,
 			}
-			return httpmock.NewJsonResponse(200, customResponseResponse)
+			return httpmock.NewJsonResponse(200, response)
 		})
 
-	httpmock.RegisterResponder("DELETE", fmt.Sprintf("%s/organizations/%s/custom-responses/%s", baseUrl, organizationID, ruleID),
+	httpmock.RegisterResponder("DELETE", fmt.Sprintf("%s/organizations/%s/projects/%s/rules/custom-response/%s", baseUrl, organizationID, organizationID, uuid),
 		func(req *http.Request) (*http.Response, error) {
 			return httpmock.NewStringResponse(204, ""), nil
 		})
+}
+
+func testCustomResponseResourceConfigMock(organizationID string, name string) string {
+	return fmt.Sprintf(`
+provider "quant" {
+	organization = %[1]q
+	bearer = "testtoken"
+}
+
+resource "quant_rule_custom_response" "test" {
+  project = %[1]q
+  name = "custom response"
+  domain = ["any"]
+  url = ["/test"]
+  custom_response_status_code = 200
+  custom_response_body = "<h1>test</h1>"
+}
+`, organizationID, name)
+}
+
+func testCustomResponseResourceConfigUpdateMock(organizationID string, name string) string {
+	return fmt.Sprintf(`
+provider "quant" {
+	organization = %[1]q
+	bearer = "testtoken"
+}
+
+resource "quant_rule_custom_response" "test" {
+  project = %[1]q
+  name = "custom response"
+  domain = ["any"]
+  url = ["/test"]
+  custom_response_status_code = 500
+  custom_response_body = "<h1>Internal Server Error</h1>"
+}
+`, organizationID, name)
 }
 
 func TestAccRuleCustomResponseResource(t *testing.T) {
@@ -106,17 +141,19 @@ func TestAccRuleCustomResponseResource(t *testing.T) {
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testCustomResponseResourceConfigMock(organizationID, ruleID, "test"),
+				Config: testCustomResponseResourceConfigMock(organizationID, "test"),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "rule_id", ruleID),
-					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "status_code", "200"),
-					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "body", "<h1>test</h1>"),
+					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "custom_response_status_code", "200"),
+					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "custom_response_body", "<h1>test</h1>"),
 				),
 			},
 			{
-				ResourceName:      "quant_rule_custom_response.test",
-				ImportState:       true,
-				ImportStateVerify: true,
+				ResourceName:            "quant_rule_custom_response.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateId:           fmt.Sprintf("%s/%s", organizationID, ruleID),
+				ImportStateVerifyIdentifierAttribute: "uuid",
 			},
 		},
 	})
@@ -132,37 +169,19 @@ func TestAccRuleCustomResponseResource_Update(t *testing.T) {
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testCustomResponseResourceConfigMock(organizationID, ruleID, "test"),
+				Config: testCustomResponseResourceConfigMock(organizationID, "test"),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "status_code", "200"),
-					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "body", "<h1>test</h1>"),
+					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "custom_response_status_code", "200"),
+					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "custom_response_body", "<h1>test</h1>"),
 				),
 			},
 			{
-				Config: testCustomResponseResourceConfigMock(organizationID, ruleID, "test"),
+				Config: testCustomResponseResourceConfigUpdateMock(organizationID, "test"),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "status_code", "500"),
-					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "body", "<h1>Internal Server Error</h1>"),
+					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "custom_response_status_code", "500"),
+					resource.TestCheckResourceAttr("quant_rule_custom_response.test", "custom_response_body", "<h1>Internal Server Error</h1>"),
 				),
 			},
 		},
 	})
-}
-
-func testCustomResponseResourceConfigMock(organizationID string, projectID string, name string) string {
-	return fmt.Sprintf(`
-provider "quant" {
-	organization = %[1]q
-	bearer = "testtoken"
-}
-
-resource "quant_rule_custom_response" "test" {
-  project = %[1]q
-  name = %[2]q
-  domain = ["example.com"]
-  url = ["/test"]
-  custom_response_status_code = 200
-  custom_response_body = "test"
-}
-`, organizationID, projectID, name)
 } 
