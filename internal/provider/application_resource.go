878416cad35e43d242014f080bb6f1f28ae6af26
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2026-04-02 12:21:02 +1000

    fix: resolve Application unknown fields for Pulumi bridge
    Set DeploymentInformation, Environments, ImageReference, Application
    to null/empty after Read to prevent unknown PropertyValue panics.

--- internal/provider/application_resource.go
+++ internal/provider/application_resource.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -416,6 +417,20 @@ func callApplicationReadAPI(ctx context.Context, r *applicationResource, data *r
 		data.ContainerNames = namesList
 	} else {
 		data.ContainerNames = types.ListNull(types.StringType)
+	}
+
+	// Computed fields that must be resolved to avoid "unknown" Pulumi bridge panic.
+	if data.DeploymentInformation.IsUnknown() {
+		data.DeploymentInformation = types.ListNull(types.ObjectType{AttrTypes: resource_application.DeploymentInformationValue{}.AttributeTypes(ctx)})
+	}
+	if data.Environments.IsUnknown() || data.Environments.IsNull() {
+		data.Environments = types.ListValueMust(types.ObjectType{AttrTypes: resource_application.EnvironmentValue{}.AttributeTypes(ctx)}, []attr.Value{})
+	}
+	if data.ImageReference.IsUnknown() {
+		data.ImageReference = resource_application.NewImageReferenceValueNull()
+	}
+	if data.Application.IsUnknown() {
+		data.Application = types.StringNull()
 	}
 
 	return
